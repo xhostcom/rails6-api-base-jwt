10a9a9d4937311a64ed07347ee39d6c72c781f97
Paul Anthony McGowan <xhostcom@gmail.com>
2021-10-15 12:36:45 +0100

    Comment out unused

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :email
-  has_many :products
+  # has_many :whatever
   cache_options enabled: true, cache_length: 12.hours
 end
